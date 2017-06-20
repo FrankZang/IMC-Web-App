147950be959ea212f6d13dc0918bea420d8c8d96
Frank Zang <fsantos.fs37@gmail.com>
2017-06-20 11:09:23 -0300

    refactor, js classes, ui tweaks

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,144 @@
-let IMC={};const inputs=document.querySelectorAll('.input');const indice=document.querySelector('#indice');const weightOut=document.querySelectorAll('.weight');const IMCOut=document.querySelector('#imc');const IMCBtn=document.querySelector('#btn');const eraseBtn=document.querySelector('.erase');const intro=document.querySelector('.intro');const result_card=document.querySelector('.result-card');function getValue(){let weight=inputs[0].value;let height=inputs[1].value;if(weight!=0&&height!=0){IMCCalculator(weight,height);}};function IMCCalculator(weight,height){if(height<=2.0){height=height||height.replace(".",",");IMC.height=height*100;}else{height=height / 100;IMC.height=height*100;}
-imc=weight /(height*height);IMC.value=imc.toString().substring(0,4)
-comparator()};function comparator(){if(IMC.height%2!==0){IMC.height=IMC.height-1;}
-function fill(){weightOut[0].innerHTML=dataTable[`${IMC.height}`].min+' kg';weightOut[1].innerHTML=dataTable[`${IMC.height}`].max+' kg';IMCOut.innerHTML=IMC.value;}
-if(IMC.value<18){indice.innerHTML=weightReference.down;fill();}else if(IMC.value<24){indice.innerHTML=weightReference.ideal;fill();}else if(IMC.value<29){indice.innerHTML=weightReference.ideal;fill();}else if(IMC.value<34){indice.innerHTML=weightReference.above;fill();}else if(IMC.value<39){indice.innerHTML=weightReference.above;fill();}else if(IMC.value>40){indice.innerHTML=weightReference.above;fill();}
-showCard()};function showCard(){intro.classList.add('intro__out');result_card.classList.add('result-card__visible');eraseBtn.classList.add('erase__active');originalState();};function eraseData(){result_card.classList.remove('result-card__visible');intro.classList.remove('intro__out');eraseBtn.classList.remove('erase__active');}
-function originalState(){inputs[0].value='';inputs[1].value='';IMCBtn.classList.remove('imc-btn__active');label[0].classList.remove('input-label__active');label[1].classList.remove('input-label__active');}
-IMCBtn.addEventListener('click',getValue);document.querySelector('#clear').addEventListener('click',eraseData);const label=document.querySelectorAll('.input-label');function focus(){this.parentNode.children[0].classList.add('input-label__active');this.parentNode.children[0].style.color='#0097a7'}
-function blur(){if(this.value==0){this.parentNode.children[0].classList.remove('input-label__active');this.parentNode.children[0].style.color='rgba(0,0,0, .5)'}
-else{this.parentNode.children[0].style.color='rgba(0,0,0, .5)'}}
-Array.prototype.forEach.call(inputs,focusChange);function focusChange(e){e.addEventListener('focus',focus);e.addEventListener('blur',blur)}
+'use strict';
+
+class Calculator {
+
+	constructor () {
+
+		this.IMC = {};
+
+		this.inputs = document.querySelectorAll('.input');
+		this.btn = document.querySelector('#btn');
+		this.weightOut = document.querySelectorAll('.weight');
+		this.imcOut = document.querySelector('#imc');
+		this.indice = document.querySelector('#indice');
+		this.eraseBtn = document.querySelector('.erase');
+		this.intro = document.querySelector('.intro');
+		this.result_card = document.querySelector('.result-card');
+		this.label = document.querySelectorAll('.input-label');
+
+		this.getValue = this.getValue.bind(this);
+		this.comparator = this.comparator.bind(this);
+		this.hideCard = this.hideCard.bind(this);
+		this.originalState = this.originalState.bind(this);
+
+		this.listeners();
+	}
+
+	listeners () {
+		this.btn.addEventListener('click', this.getValue);
+		this.eraseBtn.addEventListener('click', this.hideCard)
+	}
+
+	getValue () {
+	    let weight = this.inputs[0].value;
+	    let height = this.inputs[1].value;
+
+	    if (weight != 0 && height != 0) {
+
+		    if (height <= 2.0) {
+		        height = height;
+		        this.IMC.height = height * 100;
+		    } else {
+		        height = height / 100;
+		        this.IMC.height = height * 100;
+		    }
+
+		    let result = weight / (height * height);
+
+		    this.IMC.value = result.toString().substring(0, 4)
+
+		    this.comparator()	
+	    }
+	}
+
+	comparator () {
+	    if (this.IMC.height % 2 !== 0) {
+	        this.IMC.height = this.IMC.height - 1;
+	    }
+	    if (this.IMC.value < 18) {
+	        indice.innerHTML = weightReference.down;
+	        this.fill();
+	    } else if (this.IMC.value < 24) {
+	        indice.innerHTML = weightReference.ideal;
+	        this.fill();
+	    } else if (this.IMC.value < 29) {
+	        indice.innerHTML = weightReference.ideal;
+	        this.fill();
+	    } else if (this.IMC.value < 34) {
+	        indice.innerHTML = weightReference.above;
+	        this.fill();
+	    } else if (this.IMC.value < 39) {
+	        indice.innerHTML = weightReference.above;
+	        this.fill();
+	    } else if (this.IMC.value > 40) {
+	        indice.innerHTML = weightReference.above;
+	        this.fill();
+	    }
+
+	    this.showCard();
+	}
+
+    fill () {
+        this.weightOut[0].innerHTML = dataTable[`${this.IMC.height}`].min + ' kg';
+        this.weightOut[1].innerHTML = dataTable[`${this.IMC.height}`].max + ' kg';
+        this.imcOut.innerHTML = this.IMC.value;
+    }
+
+    showCard () {
+	    this.intro.classList.add('intro__out');
+	    this.result_card.classList.add('result-card__visible');
+	    this.eraseBtn.classList.add('erase__active');
+	    this.originalState();
+	}
+
+	hideCard () {
+	    this.result_card.classList.remove('result-card__visible');
+	    this.intro.classList.remove('intro__out');
+	    this.eraseBtn.classList.remove('erase__active');
+	}
+
+	originalState () {
+
+		for (let i = 0; i < this.inputs.length; i++){
+			console.log(i)
+			this.inputs[i].value = '';
+			this.label[i].classList.remove('input-label__active');
+		};
+}
+
+};
+
+new Calculator();
+
+class inputState {
+	constructor () {
+		this.label = document.querySelectorAll('.input-label');
+		this.inputs = document.querySelectorAll('.input');
+
+		this.focusListeners = this.focusListeners.bind(this);
+
+		Array.prototype.forEach.call(this.inputs, this.focusListeners);
+	}
+
+	focusListeners (e) {
+		e.addEventListener('focus', this.focus);
+	    e.addEventListener('blur', this.blur)
+	}
+
+	focus (e) {
+		this.parentNode.children[0].classList.add('input-label__active');
+	    this.parentNode.children[0].style.color = '#0097a7'
+	}
+
+	blur (e) {
+		if (this.value == 0) {
+	        this.parentNode.children[0].classList.remove('input-label__active');
+	        this.parentNode.children[0].style.color = 'rgba(0,0,0, .5)'
+	    }
+	    else {
+	        this.parentNode.children[0].style.color = 'rgba(0,0,0, .5)'
+	    }
+	}
+};
+
+new inputState();
